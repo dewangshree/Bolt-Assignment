af815825aa8046b68ecb474bf341fbd8334c80ca
Shreyas Dewangswami <138751486+dewangshree@users.noreply.github.com>
2026-08-15 15:20:47 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,8 +16,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         settings.FRONTEND_URL,
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
+        "https://bolt-assignment-bay.vercel.app",
+        "https://bolt-assignment-jsmgg27jn-shreyas-projects-ff372eaf.vercel.app",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
